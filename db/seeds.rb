7213637b5c177feb98545e59f90014393303fee0
Houndsto0th <bry.kinnear@gmail.com>
2015-03-16 18:31:20 -0600

    seeded DB with two users and rants to test for user 'lockout'

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,11 @@
 User.delete_all
+Rant.delete_all
 
-user = User.create(
+
+user1 = User.create(
   first_name: "Felix",
   last_name: "Tester",
-  email: "user@example.com",
+  email: "userf@example.com",
   password: "password", password_confirmation: "password"
 )
 
@@ -11,6 +13,21 @@ user = User.create(
   Rant.create!(
   title: Faker::Lorem.word,
   body: Faker::Lorem.paragraph,
-  user_id: user.id
+  user_id: user1.id
+  )
+end
+
+user2 = User.create(
+  first_name: "Mischa",
+  last_name: "Tester",
+  email: "userm@example.com",
+  password: "password", password_confirmation: "password"
+)
+
+10.times do
+  Rant.create!(
+  title: Faker::Lorem.word,
+  body: Faker::Lorem.paragraph,
+  user_id: user2.id
   )
 end
