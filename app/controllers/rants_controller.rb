2e6f16cc8d95ae6236b46585ca3dab847a6ba2cb
Houndsto0th <bry.kinnear@gmail.com>
2015-03-18 16:38:51 -0600

    fixed display of rants for fetchAll and added user controller actions

--- app/controllers/rants_controller.rb
+++ app/controllers/rants_controller.rb
@@ -1,5 +1,5 @@
 class RantsController < ApplicationController
-  before_filter :authenticate_user!, only: [:create]
+  before_filter :authenticate_user!, only: [:delete]
 
   def index
     render json: Rant.all
@@ -10,8 +10,18 @@ class RantsController < ApplicationController
   end
 
   def create
-
+    @rant = Rant.new(rant_params)
+    if @rant.save
+      render json: @rant, status: :created
+    else
+      render json: @rant.errors, status: :unprocessable_entity
+    end
   end
 
 
+
+  private
+  def rant_params
+    params.require(:rant).permit(:title, :body, :user, :user_id)
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,18 @@
 class UsersController < ApplicationController
+  before_filter :authenticate_user!, only: [:delete]
+
+  def index
+    if params[:email].present?
+     @users = User.where(email: params[:email])
+   else
+     @users = User.all
+   end
+   render json: @users
+  end
 
   def show
-    render json: User.find(params[:user_id])
+    render json: User.find(params[:id])
   end
+
 
 end
